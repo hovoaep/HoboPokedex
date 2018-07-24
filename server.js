e8991e67a7f26861eb04dbc8242b5c892edf692f
hovoaep <hovoaep@gmail.com>
2018-07-24 17:13:13 +0400

    Heroku deploy

--- server.js
+++ server.js
@@ -28,13 +28,14 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
+console.log(process.env.NODE_ENV);
 const port = process.env.PORT || 5000;
-
+console.log(process.env.NODE_ENV);
 app.listen(port, () => console.log(`Server running on port ${port}`));
