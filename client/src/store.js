3b68e351c13f4d440668731187774038335ceef4
hovoaep <hovoaep@gmail.com>
2018-07-26 21:31:00 +0400

    Fix redux error in live demo

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
@@ -9,10 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
