8c3cc50ed6f8c8c1a3184c21ff9db3020969c13f
hovoaep <hovoaep@gmail.com>
2018-07-26 22:15:43 +0400

    Fix user errors

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const randomstring = require("randomstring");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
